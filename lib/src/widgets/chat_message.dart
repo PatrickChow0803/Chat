6681080a7f90c9ce8d904e0fb12530454f407a7c
patrickchow0803 <patrickchow0803@gmail.com>
2020-11-30 16:59:12 -0500

    Date Time now displays on the message widgets

--- lib/src/widgets/chat_message.dart
+++ lib/src/widgets/chat_message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ChatMessage extends StatelessWidget {
   final int index;
@@ -9,24 +10,34 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            constraints: BoxConstraints(maxWidth: 300.0),
-            decoration:
-                BoxDecoration(color: Colors.blueGrey, borderRadius: BorderRadius.circular(5)),
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
-            child: Text(
-              data['value'],
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
-      ),
+    int timeInMillis = data['timestamp'];
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(timeInMillis);
+    String formattedDate = DateFormat.yMMMd().format(date);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                constraints: BoxConstraints(maxWidth: 300.0),
+                decoration:
+                    BoxDecoration(color: Colors.blueGrey, borderRadius: BorderRadius.circular(5)),
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+                child: Text(
+                  data['value'],
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(margin: EdgeInsets.only(right: 10, top: 0), child: Text(formattedDate)),
+      ],
     );
   }
 }
